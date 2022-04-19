a45ff9a997acc313f233078db10178e05f2bd37d
Shankar Lohar <shankarloharmail@gmail.com>
2022-04-19 10:14:40 +0530

    Phase 2 on process

--- app/src/main/java/com/shankarlohar/personalbudgetingapp/MainActivity.java
+++ app/src/main/java/com/shankarlohar/personalbudgetingapp/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CardView budgetCardView;
+    private CardView budgetCardView, todayCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         budgetCardView = findViewById(R.id.budgetCardview);
+        todayCardView = findViewById(R.id.todayCardview);
 
         budgetCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, BudgetActivity.class));
+            }
+        });
+
+        todayCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, TodaySpendingActivity.class));
             }
         });
 
